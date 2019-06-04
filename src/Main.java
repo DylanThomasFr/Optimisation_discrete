94345b7b2295201c5b9b502d0f46a645ae3fb8a9
SUARD Tancrède <tancrede.suard@etu.univ-lyon1.fr>
2019-06-04 15:56:15 +0200

    SilumatedAnnaeling fonctionne

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import algo.HillClimbing;
 import algo.RandomWalk;
+import algo.SimulatedAnnealing;
 import io.Parser;
 import utils.Landscape;
 import utils.Order;
@@ -9,10 +10,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Landscape landscape = Parser.createLandscape("data/tai12a.dat");
 
-//        Order solutionOrder = new Order("8,1,6,2,11,10,3,5,9,7,12,4");
+        Order solutionOrder = new Order("8,1,6,2,11,10,3,5,9,7,12,4");
 
-//        long bestSolutionFitness = landscape.computeFitness(solutionOrder);
-//        System.out.println("bestSolutionFitness = " + bestSolutionFitness);
+        long bestSolutionFitness = landscape.computeFitness(solutionOrder);
+        System.out.println("bestSolutionFitness = " + bestSolutionFitness);
 
 //        RandomWalk randomWalk = new RandomWalk(1000);
 //        Order bestComputedOrder = randomWalk.compute(landscape);
@@ -20,11 +21,19 @@ public class Main {
 //        long bestComputedFitness = landscape.computeFitness(bestComputedOrder);
 //        System.out.println("bestComputedFitness = " + bestComputedFitness);
 
-        HillClimbing hillClimbing = new HillClimbing(10000);
-        Order bestComputedOrder = hillClimbing.compute(landscape);
+//        HillClimbing hillClimbing = new HillClimbing(10000);
+//        Order bestComputedOrder = hillClimbing.compute(landscape);
+//        System.out.println("bestComputedOrder = " + bestComputedOrder);
+//        long bestComputedFitness = landscape.computeFitness(bestComputedOrder);
+//        System.out.println("bestComputedFitness = " + bestComputedFitness);
+
+
+        SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing( 0.99F, 30000);
+        Order bestComputedOrder = simulatedAnnealing.compute(landscape);
         System.out.println("bestComputedOrder = " + bestComputedOrder);
         long bestComputedFitness = landscape.computeFitness(bestComputedOrder);
         System.out.println("bestComputedFitness = " + bestComputedFitness);
+
 
 //        int i = 1000000;
 //        int newValue;
--- src/algo/SimulatedAnnealing.java
+++ src/algo/SimulatedAnnealing.java
@@ -1,14 +1,94 @@
 package algo;
 
+import algo.mapping.MPermutation;
+import javafx.util.Pair;
 import utils.Landscape;
 import utils.Order;
 
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+
 public class SimulatedAnnealing implements IAlgo {
 
-//    public SimulatedAnnealing(int maxSteps, int temperature, )
+    private final float mu;
+    private final Order initialSolution;
+    private final int movesByTemperature;
+
+    private Random rand = new Random();
+    private MPermutation mapping = new MPermutation();
+
+    public SimulatedAnnealing(final float mu, final int movesByTemperature) {
+        this(mu, movesByTemperature,null);
+    }
+
+    public SimulatedAnnealing(final float mu, final int movesByTemperature, final Order initialSolution){
+        this.mu = mu;
+        this.initialSolution = initialSolution;
+        this.movesByTemperature = movesByTemperature;
+    }
 
     @Override
     public Order compute(Landscape landscape) {
-        return null; //TODO
+        Order currentSolution;
+        if(initialSolution == null){
+            currentSolution = new Order(landscape.getSIZE());
+            currentSolution.shuffle();
+        } else currentSolution = initialSolution;
+
+        Order bestSolution = currentSolution.clone();
+        long bestFitness = landscape.computeFitness(bestSolution);
+        Pair<Float, Integer> startingPoints = processTemperature0(landscape, currentSolution, 0.8D);
+
+        Order solution;
+        Order nextSolution;
+        int maxChangesOfTemperature = startingPoints.getValue();
+        float temperature = startingPoints.getKey();
+
+        System.out.println("maxChangesOfTemperature = " + maxChangesOfTemperature);
+        System.out.println("temperature = " + temperature);
+
+        for (int k = 0; k < maxChangesOfTemperature; k++) {
+            for (int l = 1; l < movesByTemperature; l++) {
+                List<Order> neighbors = mapping.getNeighbors(currentSolution);
+                solution = neighbors.get(rand.nextInt(neighbors.size())).clone();
+                float deltaFitness = landscape.computeFitness(solution) - landscape.computeFitness(currentSolution);
+                if(deltaFitness <= 0){
+                    nextSolution = solution.clone();
+                    if(landscape.computeFitness(nextSolution) < bestFitness){
+                        bestSolution = nextSolution.clone();
+                        bestFitness = landscape.computeFitness(bestSolution);
+                    }
+                } else {
+                    float p = rand.nextFloat();
+                    if(p < Math.exp(-deltaFitness / temperature)){
+                        nextSolution = solution.clone();
+                    } else {
+                        nextSolution = currentSolution;
+                    }
+                }
+                currentSolution = nextSolution;
+            }
+            temperature = mu * temperature;
+        }
+        return bestSolution;
+    }
+
+    private Pair<Float, Integer> processTemperature0(Landscape landscape, Order initialSolution, final double probaChances){
+        long worstFitness = Long.MIN_VALUE;
+        for (Order solution : mapping.getNeighbors(initialSolution)) {
+            long fitness = landscape.computeFitness(solution);
+            if(fitness > worstFitness){
+                worstFitness = fitness;
+            }
+        }
+        long deltaFitness = worstFitness - landscape.computeFitness(initialSolution);
+        float temperature = -deltaFitness / (float) Math.log(probaChances);
+        int temperatureChanges = getTemperatureChanges(deltaFitness, temperature, 0.01);
+        return new Pair<>(temperature, temperatureChanges);
+    }
+
+    private int getTemperatureChanges(final float deltaFitness, float temperature0, final double proba){
+        return (int) (Math.log(-deltaFitness / (temperature0 * Math.log(proba))) / Math.log(mu)) + 1;
     }
 }
