d0fefa7a05ea831ef52d2896056ec6d4b8a59d3e
SUARD Tancrède <tancrede.suard@etu.univ-lyon1.fr>
2019-06-05 22:25:54 +0200

    Benchmark improvments logging time processing

--- src/benchmark/BenchJob.java
+++ src/benchmark/BenchJob.java
@@ -10,27 +10,37 @@ public class BenchJob implements Runnable{
     private final Landscape landscape;
     private final IAlgo algo;
     private final HashMap<Landscape, HashMap<IAlgo, Float>> returnFitness;
+    private final HashMap<Landscape, HashMap<IAlgo, Long>> returnTimeComputed;
     private final int avgIter;
 
     public BenchJob(
             final Landscape landscape,
             final IAlgo algo,
             final HashMap<Landscape, HashMap<IAlgo, Float>> returnFitness,
+            final HashMap<Landscape, HashMap<IAlgo, Long>> returnTimeComputed,
             int avgIter
             ){
         this.landscape = landscape;
         this.algo = algo;
         this.returnFitness = returnFitness;
+        this.returnTimeComputed = returnTimeComputed;
         this.avgIter = avgIter;
     }
 
     @Override
     public void run() {
         long fitness = 0;
+        long startTime;
+        long stopTime;
+        long time = 0;
         for (int i = 0; i < avgIter; i++) {
+            startTime = System.nanoTime();
             fitness+= landscape.computeFitness(algo.compute(landscape));
+            stopTime = System.nanoTime();
+            time += stopTime - startTime;
         }
         fitness = Math.round(fitness / avgIter);
         returnFitness.get(landscape).put(algo, (float) fitness / landscape.getBestFitness());
+        returnTimeComputed.get(landscape).put(algo, time / (avgIter * 1000));
     }
 }
