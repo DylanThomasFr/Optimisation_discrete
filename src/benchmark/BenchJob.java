21da09c5ac1171a0746ab4ad767eca85c4fb9ffd
SUARD Tancrède <tancrede.suard@etu.univ-lyon1.fr>
2019-06-05 15:10:17 +0200

    Benchmarks

---
+++ src/benchmark/BenchJob.java
@@ -0,0 +1,36 @@
+package benchmark;
+
+import algo.IAlgo;
+import utils.Landscape;
+
+import java.util.HashMap;
+
+public class BenchJob implements Runnable{
+
+    private final Landscape landscape;
+    private final IAlgo algo;
+    private final HashMap<Landscape, HashMap<IAlgo, Long>> returnFitness;
+    private final int avgIter;
+
+    public BenchJob(
+            final Landscape landscape,
+            final IAlgo algo,
+            final HashMap<Landscape, HashMap<IAlgo, Long>> returnFitness,
+            int avgIter
+            ){
+        this.landscape = landscape;
+        this.algo = algo;
+        this.returnFitness = returnFitness;
+        this.avgIter = avgIter;
+    }
+
+    @Override
+    public void run() {
+        long fitness = 0;
+        for (int i = 0; i < avgIter; i++) {
+            fitness+= landscape.computeFitness(algo.compute(landscape));
+        }
+        fitness = Math.round(fitness / avgIter);
+        returnFitness.get(landscape).put(algo, fitness);
+    }
+}
--- src/benchmark/Benchmark.java
+++ src/benchmark/Benchmark.java
@@ -1,4 +1,77 @@
 package benchmark;
 
+import algo.IAlgo;
+import algo.mapping.IMapping;
+import javafx.util.Pair;
+import utils.Landscape;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Benchmark {
+
+    private final int avgIter;
+    private HashSet<Landscape> landscapes;
+    private HashSet<IAlgo> algos;
+
+    private HashMap<Landscape, HashMap<IAlgo, Long>> results;
+
+    public Benchmark(int avgIter){
+        this.avgIter = avgIter;
+//        returnFirtness = new LinkedBlockingQueue<>();
+        results = new HashMap<>();
+    }
+
+    public Benchmark registerLandscape(Landscape landscape){
+        landscapes.add(landscape);
+        return this;
+    }
+
+    public Benchmark registerAlgo(IAlgo algo){
+        algos.add(algo);
+        return this;
+    }
+
+    public void runBench(){
+        for (Landscape lanscape : landscapes) {
+            results.put(lanscape, new HashMap<>());
+            for (IAlgo algo : algos) {
+                results.get(lanscape).put(algo, 0L);
+            }
+        }
+
+        ExecutorService es = Executors.newFixedThreadPool(4);
+        List<BenchJob> tasks = getBenchJobs();
+        CompletableFuture<?>[] futures = tasks.stream()
+                .map(task -> CompletableFuture.runAsync(task, es))
+                .toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(futures).join();
+        es.shutdown();
+
+        for (Landscape lanscape : landscapes) {
+            for (IAlgo algo : algos) {
+                System.out.println("results("+lanscape+")("+algo+") " + results.get(lanscape).get(algo));
+            }
+        }
+    }
+
+    public List<BenchJob> getBenchJobs(){
+        List<BenchJob> benchJobs = new ArrayList<>();
+        for (Landscape landscape : landscapes) {
+            for (IAlgo algo : algos) {
+                benchJobs.add(
+                        new BenchJob(
+                                landscape,
+                                algo,
+                                results,
+                                avgIter
+                                )
+                );
+            }
+        }
+        return benchJobs;
+    }
 }
