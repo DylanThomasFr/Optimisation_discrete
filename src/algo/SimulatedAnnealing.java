94d09189f157321cd3e68c16184e290a507c8d74
SUARD Tancrède <tancrede.suard@etu.univ-lyon1.fr>
2019-06-08 12:43:38 +0200

    Ajout de nouveaux tests

--- src/algo/SimulatedAnnealing.java
+++ src/algo/SimulatedAnnealing.java
@@ -14,19 +14,26 @@ public class SimulatedAnnealing implements IAlgo {
     private final float mu;
     private final Order initialSolution;
     private final int movesByTemperature;
+    private final float proboAcceptBadSolutions;
+    private final float probaAcceptThatBadSolution;
 
     private Random rand = new Random();
     private final IMapping mapping;
 
-    public SimulatedAnnealing(final IMapping mapping, final float mu, final int movesByTemperature) {
-        this(mapping, mu, movesByTemperature,null);
+    public SimulatedAnnealing(final IMapping mapping, final float mu, final int movesByTemperature,
+                              final float proboAcceptBadSolutions, final float probaAcceptThatBadSolution) {
+        this(mapping, mu, movesByTemperature,proboAcceptBadSolutions, probaAcceptThatBadSolution, null);
     }
 
-    public SimulatedAnnealing(final IMapping mapping, final float mu, final int movesByTemperature, final Order initialSolution){
+    public SimulatedAnnealing(final IMapping mapping, final float mu, final int movesByTemperature,
+                              final float proboAcceptBadSolutions, final float probaAcceptThatBadSolution,
+                              final Order initialSolution){
         this.mapping = mapping;
         this.mu = mu;
         this.initialSolution = initialSolution;
         this.movesByTemperature = movesByTemperature;
+        this.proboAcceptBadSolutions = proboAcceptBadSolutions;
+        this.probaAcceptThatBadSolution = probaAcceptThatBadSolution;
     }
 
     @Override
@@ -40,7 +47,7 @@ public class SimulatedAnnealing implements IAlgo {
 
         Order bestSolution = currentSolution.clone();
         long bestFitness = landscape.computeFitness(bestSolution);
-        Pair<Float, Integer> startingPoints = processTemperature0(landscape, currentSolution, 0.8D);
+        Pair<Float, Integer> startingPoints = processTemperature0(landscape, currentSolution);
 
         Order solution;
         Order nextSolution;
@@ -73,7 +80,7 @@ public class SimulatedAnnealing implements IAlgo {
         return bestSolution;
     }
 
-    private Pair<Float, Integer> processTemperature0(Landscape landscape, Order initialSolution, final double probaChances){
+    private Pair<Float, Integer> processTemperature0(Landscape landscape, Order initialSolution){
         long worstFitness = Long.MIN_VALUE;
         for (Order solution : mapping.getNeighbors(initialSolution)) {
             long fitness = landscape.computeFitness(solution);
@@ -82,13 +89,13 @@ public class SimulatedAnnealing implements IAlgo {
             }
         }
         long deltaFitness = worstFitness - landscape.computeFitness(initialSolution);
-        float temperature = -deltaFitness / (float) Math.log(probaChances);
-        int temperatureChanges = getTemperatureChanges(deltaFitness, temperature, 0.01);
+        float temperature = -deltaFitness / (float) Math.log(proboAcceptBadSolutions);
+        int temperatureChanges = getTemperatureChanges(deltaFitness, temperature);
         return new Pair<>(temperature, temperatureChanges);
     }
 
-    private int getTemperatureChanges(final float deltaFitness, float temperature0, final double proba){
-        return (int) (Math.log(-deltaFitness / (temperature0 * Math.log(proba))) / Math.log(mu)) + 1;
+    private int getTemperatureChanges(final float deltaFitness, float temperature0){
+        return (int) (Math.log(-deltaFitness / (temperature0 * Math.log(probaAcceptThatBadSolution))) / Math.log(mu)) + 1;
     }
 
     @Override
@@ -96,7 +103,10 @@ public class SimulatedAnnealing implements IAlgo {
         return "SimulatedAnnealing,"+mapping+
                 ",mu,"+mu+
                 ",movesByTemperature,"+movesByTemperature+
-                ",initialSolution,"+(initialSolution == null ? "false" : "true");
+                ",initialSolution,"+(initialSolution == null ? "0" : "1" +
+                ",proboAcceptBadSolutions,"+proboAcceptBadSolutions+
+                ",probaAcceptThatBadSolution,"+probaAcceptThatBadSolution
+        );
     }
 
     @Override
